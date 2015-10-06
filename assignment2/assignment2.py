e6b06a77c7fd3735e614bfa1cebfd61b4a415fdd
unknown <rene1047@gmail.com>
2015-10-06 13:35:27 +0300

    assignment2 not found

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -5,7 +5,7 @@ def hello_name(name):
       
 # Given two strings, a and b, return the result 
 # of putting them together in the order abba, 
-# e.g. "Hi" and "Bye" returns "HiByeByeHi". 
+# e.g. "Hi" and "Bye" returns "HiByeByeHi".
 def make_abba(a, b):
     result2 = "{0}{1}{1}{0}".format(a,b)
     return result2 
