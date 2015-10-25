f480b4a153df54fd3ec8d83f91a47fd1f21f1a0e
unknown <rene1047@gmail.com>
2015-10-25 16:56:50 +0200

    parandused

--- TeineiseseisevYlesanne/TeineIseseisevYlesanne.py
+++ TeineiseseisevYlesanne/TeineIseseisevYlesanne.py
@@ -1,47 +1,32 @@
-#-----------------------Funktsioonid---------------------------
+
 def kirjavahemargid(x):
-    a=False
+    a=bool()
     for char in x:
         if char in string.punctuation: a=True
     return a
-def taisarvulinevaartus(y):
-    b=False
-    if y.isdigit()==True: b=True
-    return b
-def kokkuvote(x,y,z,t):
-    t=map(int,t)
-    eiPeatunud=sum(t)-(x+y+z)
-    c=print('''
-Endla järvele on maandunud kokku {0} hane!
-Saadjärvele on maandunud kokku {1} hane!
-Ratva järvele on maandunud kokku {2} hane!
-Eesti järvedel ei peatunud {3} hane!'''.format(x,y,z,eiPeatunud))
-    return c
-def sisendkunitaisarv(nimi,kysimus):
-    while taisarvulinevaartus(nimi) != True:
-        nimi=input(kysimus)
-    nimi = int(nimi)
-    return nimi
-    
-#-----------------------------Kood---------------------------   
+ 
 sisend = 'jah'
 while sisend == 'jah':
     import string
-    nimi=''
-    mituParve=sisendkunitaisarv(nimi,'Mitu haneparve on nähtud? ')
+    mituParve=''
+    while not mituParve.isdigit():
+        mituParve = input('Mitu haneparve on nähtud? ')
+    mituParve=int(mituParve)
     arv=1
     nimed=list()
     liikmeteArv=list()
     nimedeList='1'
     LiikmeteArvuList=''
     while mituParve != arv-1:
-        nimedeList=input('''    Sisestage {0}. haneparve...
-        ...nimi: '''.format(arv))  
-        while kirjavahemargid(nimedeList)==True or taisarvulinevaartus(nimedeList)==True:
-            nimedeList=input('''    Sisestage {0}. haneparve...
-        ...nimi: '''.format(arv))
-        while taisarvulinevaartus(LiikmeteArvuList)==False:
-            LiikmeteArvuList=input('        ...liikmete arv: ')
+                
+        nimedeList=input('''sisestage {0}.
+haneparve nimi:'''.format(arv))
+
+        while kirjavahemargid(nimedeList)==True or nimedeList.isdigit() == True:
+            nimedeList=input('''sisestage {0}.
+haneparve nimi:'''.format(arv))
+        while LiikmeteArvuList.isdigit()==False:
+            LiikmeteArvuList=input('liikmete arv: ')
         nimed.append(nimedeList)
         liikmeteArv.append(LiikmeteArvuList)
         arv+=1
@@ -50,10 +35,11 @@ while sisend == 'jah':
     indeks=0
     print('Lõunasse lähevad järgmised haneparved: ')
     while arv != indeks:
-        print(' {0} ({1} hane)'.format(nimed[indeks],liikmeteArv[indeks]))
+        print('{0} ({1})'.format(nimed[indeks],liikmeteArv[indeks]))
         indeks+=1
 	
-    indeks=0	
+    indeks=0
+	
     kokkuRatva=0
     kokkuEndla=0
     kokkuSaad=0
@@ -61,52 +47,48 @@ while sisend == 'jah':
         endlaJarv=''
         saadJarv=''
         ratvaJarv=''
-
-        while taisarvulinevaartus(endlaJarv) is False or int(endlaJarv) > int(liikmeteArv[indeks]): 
+        while endlaJarv.isdigit() is False: 
             endlaJarv=input('Mitu hane maandub {0} parvest Endla järvele? '.format(nimed[indeks]))
-            if taisarvulinevaartus(endlaJarv) is True:
+            if endlaJarv.isdigit() is True:
                 if int(endlaJarv) > int(liikmeteArv[indeks]):
                     print('''   Parves {0} ei ole nii palju hanesid!
     Parves {0} on {1} hane!
     Sisestage väiksem väärtus!'''.format(nimed[indeks],liikmeteArv[indeks]))
         lendabSaad=int(liikmeteArv[indeks])-int(endlaJarv)
-        print('   Endla järvele maandus {0} hane ja {1} lendab edasi.'.format(endlaJarv,lendabSaad))
-
-        
-        while taisarvulinevaartus(saadJarv) is False or int(saadJarv)>lendabSaad:
+        print('Endla järvele maandus {0} hane ja {1} lendab edasi'.format(endlaJarv,lendabSaad))
+        while saadJarv.isdigit() is False:
             saadJarv=input('Mitu hane maandub {0} parvest Saadjärvele? '.format(nimed[indeks]))
-            if taisarvulinevaartus(saadJarv) is True:
+            if saadJarv.isdigit() is True:
                 if int(saadJarv) > lendabSaad:
                     print('''   Parves {0} ei ole nii palju hanesid!
     Parves {0} on {1} hane!
     Sisestage väiksem väärtus!'''.format(nimed[indeks],lendabSaad))
         lendabRatva=lendabSaad-int(saadJarv)
-        print('   Saadjärvele maandus {0} hane ja {1} lendab edasi.'.format(saadJarv,lendabRatva))
-        
-        while taisarvulinevaartus(ratvaJarv) is False or int(ratvaJarv)>lendabRatva:
+        print('Saadjärvele maandus {0} hane ja {1} lendab edasi'.format(saadJarv,lendabRatva))
+        while ratvaJarv.isdigit() is False:
             ratvaJarv=input('Mitu hane maandub {0} parvest Ratva järvele? '.format(nimed[indeks]))
-            if taisarvulinevaartus(ratvaJarv) is True:
+            if ratvaJarv.isdigit() is True:
                 if int(ratvaJarv) > lendabRatva:
                     print('''   Parves {0} ei ole nii palju hanesid!
     Parves {0} on {1} hane!
     Sisestage väiksem väärtus!'''.format(nimed[indeks],lendabRatva))
         lendabEdasi=lendabRatva-int(ratvaJarv)
-        print('   Ratvajärvele maandus {0} hane ja {1} hane lendab edasi.'.format(ratvaJarv,lendabEdasi))
-        
+        print('Ratvajärvele maandus {0} hane ja {1} hane lendab edasi'.format(ratvaJarv,lendabEdasi))
         indeks+=1
-        kokkuRatva+=int(ratvaJarv)
-        kokkuSaad+=int(saadJarv)
-        kokkuEndla+=int(endlaJarv)
-    kokkuvote(kokkuRatva,kokkuSaad,kokkuEndla,liikmeteArv)
-	
-    sisend=input('\nKas te soovite jätkata(jah/ei)?')
-    if sisend == jah:
-        print('\n')
+        kokkuRatva=kokkuRatva+int(ratvaJarv)
+        kokkuSaad=kokkuSaad+int(saadJarv)
+        kokkuEndla=kokkuEndla+int(endlaJarv)
+    liikmeteArv=map(int,liikmeteArv)
+    eiPeatunud=sum(liikmeteArv)-(kokkuRatva+kokkuSaad+kokkuEndla)
+    print('''Endla järvele on maandunud kokku {0} hane!
+    Saadjärvele on maandunud kokku {1} hane!
+    Ratva järvele on maandunud kokku {2} hane!
+    Eesti järvedel ei peatunud {3} hane!'''.format(kokkuEndla,kokkuSaad,kokkuRatva,eiPeatunud))
+    
+    sisend=input('Kas te soovite jätkata(jah/ei)? ')
+    
 print('Programm läks kinni!')
 exit
-
-
-
 
 
 
