c0923d245f08eb17f7b4a7f910447f15cad4117b
unknown <rene1047@gmail.com>
2015-10-26 11:27:34 +0200

    proov

--- TeineiseseisevYlesanne/TeineIseseisevYlesanne.py
+++ TeineiseseisevYlesanne/TeineIseseisevYlesanne.py
@@ -8,13 +8,13 @@ def kirjavahemargid(x):
     return a
 def haneparvedjaliikmed(x1,x2,x3,x4,x5,x6):
     while x1 != x2-1:
-        x6=input('''    Sisestage {0}. haneparve...
-        ...nimi: '''.format(x2))
+        x6=input('''Sisestage {0}. haneparve...
+...nimi: '''.format(x2))
         while kirjavahemargid(x6)==True or x6.isdigit() == True or x6 == '':
-            x6=input('''    Sisestage {0}. haneparve...
-        ...nimi: '''.format(x2))
+            x6=input('''Sisestage {0}. haneparve...
+...nimi: '''.format(x2))
         while x4.isdigit()==False or x4=='0':
-            x4=input('        ...liikmete arv: ')
+            x4=input('...liikmete arv: ')
         x5.append(x6)
         x3.append(x4)
         x2+=1
@@ -24,15 +24,14 @@ def parveLend(y1,y2,y3,y4,y5):
         y1=input("Mitu hane maandub '{0}' parvest {1}? ".format(y3[indeks],y5))
         if y1.isdigit() is True:
             if int(y1)> int(y2):
-                print('''    Parves '{0}' ei ole nii palju hanesid!
-    Parves '{0}' on {1} hane!
-    Sisestage väiksem väärtus!'''.format(y3[indeks],y2))
+                print('''Parves '{0}' ei ole nii palju hanesid!
+Parves '{0}' on {1} hane!
+Sisestage väiksem väärtus!'''.format(y3[indeks],y2))
     return y1
 def kokkuvote(x,y,z,t):
     t=map(int,t)
     eiPeatunud=sum(t)-(x+y+z)
-    c=print('''
-Endla järvele on maandunud kokku {0} hane!
+    c=print('''Endla järvele on maandunud kokku {0} hane!
 Saadjärvele on maandunud kokku {1} hane!
 Ratva järvele on maandunud kokku {2} hane!
 Eesti järvedel ei peatunud {3} hane!'''.format(x,y,z,eiPeatunud))
@@ -57,7 +56,7 @@ while sisend == 'jah':
     indeks=0
     print('Lõunasse lähevad järgmised haneparved: ')
     while arv != indeks:
-        print("    '{0}' ({1})".format(nimed[indeks],liikmeteArv[indeks]))
+        print("'{0}' ({1})".format(nimed[indeks],liikmeteArv[indeks]))
         indeks+=1	
     indeks=0
     kokkuRatva=0
@@ -73,15 +72,15 @@ while sisend == 'jah':
 
         endlaJarv=parveLend(endlaJarv,liikmeteArv[indeks],nimed,lendabSaad,'Endla järvele')
         lendabSaad=int(liikmeteArv[indeks])-int(endlaJarv)
-        print('    Endla järvele maandus {0} hane ja {1} lendab edasi!'.format(endlaJarv,lendabSaad))
+        print('Endla järvele maandus {0} hane ja {1} lendab edasi!'.format(endlaJarv,lendabSaad))
         
         saadJarv=parveLend(saadJarv,lendabSaad,nimed,lendabRatva,'Saadjärvele')
         lendabRatva=int(lendabSaad)-int(saadJarv)
-        print('    Saadjärvele maandus {0} hane ja {1} lendab edasi!'.format(saadJarv,lendabRatva))
+        print('Saadjärvele maandus {0} hane ja {1} lendab edasi!'.format(saadJarv,lendabRatva))
         
         ratvaJarv=parveLend(ratvaJarv,lendabRatva,nimed,lendabEdasi,'Ratva järvele')
         lendabEdasi=int(lendabRatva)-int(ratvaJarv)
-        print('    Ratva järvele maandus {0} hane ja {1} hane lendab edasi!'.format(ratvaJarv,lendabEdasi))
+        print('Ratva järvele maandus {0} hane ja {1} hane lendab edasi!'.format(ratvaJarv,lendabEdasi))
 
         indeks+=1
         kokkuRatva=kokkuRatva+int(ratvaJarv)
