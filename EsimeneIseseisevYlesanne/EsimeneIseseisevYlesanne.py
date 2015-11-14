d36c56c374ce0a802441a721875120e169ae1719
unknown <rene1047@gmail.com>
2015-11-14 22:28:14 +0200

    uus proov

--- EsimeneIseseisevYlesanne/EsimeneIseseisevYlesanne.py
+++ EsimeneIseseisevYlesanne/EsimeneIseseisevYlesanne.py
@@ -7,14 +7,14 @@ if sisend1 == 'ei':                                                             
 rohk = 1.80                             
 if sisend1 == 'jah':                
     print('''                           
-    Süvaveepump käivitus! 
-        Hüdrofoorirõhk: ''')                                                                                    #If first input was 'jah', then program the continues and
+\tSüvaveepump käivitus! 
+\t\tHüdrofoorirõhk: ''')                                                                                    #If first input was 'jah', then program the continues and
     while rohk<=2.75 and rohk<=2.85:                                                                    #displays pressure change in hydrophore until it reaches
-        if rohk == 1.80:print('            %.2f' %rohk , str('bar'))                                                #maximum pressure(2.8 +/-0.05)
+        if rohk == 1.80:print('\t\t\t%.2f' %rohk , str('bar'))                                                #maximum pressure(2.8 +/-0.05)
         rohk += 0.15                                                                                     
-        print('            %.2f' %rohk , str('bar'))                                                                
+        print('\t\t\t%.2f' %rohk , str('bar'))                                                                
         
-    print('    Süvaveepump seiskus!\n')    
+    print('\tSüvaveepump seiskus!\n')    
     rohk='%.2f' %rohk                                                                                   #2 digits after decimal point                                                                  
     rohk = float(rohk)                                                                                  #Float value to rohk again
     sisend2 = input('Kas keerata kraanid lahti (jah/ei)? ')                                    #Second input
@@ -34,7 +34,7 @@ if sisend1 == 'jah':
                 if veeTarbimine== '[Tarbitakse]'and 1.80<=rohk: rohk -= 0.05
                 rohk = round(rohk,2)
                 if veeTarbimine== '[Ei tarbita]':rohuSuund = '[Seisab]'
-                print('    ',pumbaOlek, veeTarbimine, rohuSuund,'%.2f' %rohk,bar,)                                             
+                print('\t',pumbaOlek, veeTarbimine, rohuSuund,'%.2f' %rohk,bar,)                                             
                 #would add time.sleep(1) here and into the other loop, but since it is a simulation, it is not necessary
                
             while pumbaOlek == '[Töötab]' and rohk<= 2.75:              
@@ -43,7 +43,7 @@ if sisend1 == 'jah':
                 if veeTarbimine== '[Ei tarbita]' and rohk <= 2.70 : rohk += 0.15                                                         
                 if veeTarbimine== '[Tarbitakse]'and rohk <= 2.75: rohk += 0.10    
                 rohk = round(rohk,2)
-                print('    ',pumbaOlek, veeTarbimine, rohuSuund,'%.2f' %rohk,bar)
+                print('\t',pumbaOlek, veeTarbimine, rohuSuund,'%.2f' %rohk,bar)
                 
                 
 
