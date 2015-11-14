432fa7e0803b126f457e41ea8b949e84f8c5545b
unknown <rene1047@gmail.com>
2015-11-15 01:19:45 +0200

    uus proov

--- TeineiseseisevYlesanne/TeineIseseisevYlesanne.py
+++ TeineiseseisevYlesanne/TeineIseseisevYlesanne.py
@@ -6,7 +6,7 @@ def haneparvenimed():
     while arv-1 != mituParve:   
         print("\tSisestage {0}. haneparve ...".format(arv))
         nimi=input("\t\t... nimi: ")
-        while nimi.isdigit() is True:
+        while nimi.isdigit() is True or kirjaVaheMark(nimi) is True:
             nimi=input("\t\t... nimi: ")          
         liikmeteArv=input("\t\t... liikmete arv: ")
         while liikmeteArv.isdigit() is False:
