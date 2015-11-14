3a9bfd1f01311f709ee8fed9d0e75b92a2881fcd
unknown <rene1047@gmail.com>
2015-11-14 21:21:16 +0200

    uus proov

--- TeineiseseisevYlesanne/TeineIseseisevYlesanne.py
+++ TeineiseseisevYlesanne/TeineIseseisevYlesanne.py
@@ -3,16 +3,16 @@ def haneparvenimed():
     nimedeList=list()
     liikmeteArvuList=list()
     while arv-1 != mituParve:
-        print("\n    Sisestage {0}. haneparve ...".format(arv))
-        nimi=input("\n        ...nimi: ")
-        liikmeteArv=input("\n        ...liikmete arv: ")
+        print("    Sisestage {0}. haneparve ...".format(arv))
+        nimi=input("        ...nimi:    ")
+        liikmeteArv=input("        ...liikmete arv:    ")
         nimedeList.append(nimi)
         liikmeteArvuList.append(liikmeteArv)
         arv+=1
     return {'nimedeList':nimedeList, 'liikmeteArvuList':liikmeteArvuList}
 
 def lounasseLahevad():
-    print("Lõunasse lähevad järgmised haneparved:\n")
+    print("Lõunasse lähevad järgmised haneparved:")
     arv=0
     while arv != int(mituParve):
         print("    ","'{0}'({1}) hane".format(nimedeList[arv],liikmeteArvuList[arv]))
@@ -24,17 +24,16 @@ def maandumine():
     kokkuMaandubSaad = 0
     kokkuMaandubRatva = 0
     kokkuLendabEdasi = 0
-    print("")
     while arv != len(nimedeList):
-        mituMaandubEndla = input("Mitu hane maandub '{0}' parvest Endla järvele? ".format(nimedeList[arv]))
+        mituMaandubEndla = input("Mitu hane maandub '{0}' parvest Endla järvele?    ".format(nimedeList[arv]))
         lendabSaad = int(liikmeteArvuList[arv]) - int(mituMaandubEndla)
         kontroll(arv,lendabSaad)
         vastus("x",mituMaandubEndla,lendabSaad)
-        mituMaandubSaad = input("Mitu hane maandub '{0}' parvest Saadjärvele? ".format(nimedeList[arv]))
+        mituMaandubSaad = input("Mitu hane maandub '{0}' parvest Saadjärvele?    ".format(nimedeList[arv]))
         lendabRatva = lendabSaad - int(mituMaandubSaad)
         kontroll(arv,lendabRatva)
         vastus("y",mituMaandubSaad,lendabRatva)
-        mituMaandubRatva =input("Mitu hane maandub '{0}' parvest Ratva järvele? ".format(nimedeList[arv]))
+        mituMaandubRatva =input("Mitu hane maandub '{0}' parvest Ratva järvele?    ".format(nimedeList[arv]))
         lendabEedasi = lendabRatva - int(mituMaandubRatva)
         kontroll(arv,lendabEedasi)
         vastus("z",mituMaandubRatva,lendabEedasi)
@@ -46,13 +45,9 @@ def maandumine():
         arv+=1
         
     print('''
-
 Endla järvele on maandunud kokku {0} hane!
-
 Saadjärvele on maandunud kokku {1} hane!
-
 Ratva järvele on maandunud kokku {2} hane!
-
 Eesti järvedel ei peatunud {3} hane!
 '''.format(kokkuMaandubEndla,kokkuMaandubSaad,kokkuMaandubRatva,kokkuLendabEdasi))
                                 
@@ -60,22 +55,23 @@ Eesti järvedel ei peatunud {3} hane!
 
 def vastus(identifikaator,mituMaandub,lendabEdasi):
     if identifikaator == "x":
-        print("\n    Endla järvele maandus {0} hane ja {1} hane lendab edasi!".format(mituMaandub,lendabEdasi))                                            
+        print("    Endla järvele maandus {0} hane ja {1} hane lendab edasi!".format(mituMaandub,lendabEdasi))                                            
     if identifikaator == "y":
-        print("\n    Saadjärvele maandus {0} hane ja {1} hane lendab edasi!".format(mituMaandub,lendabEdasi))
+        print("    Saadjärvele maandus {0} hane ja {1} hane lendab edasi!".format(mituMaandub,lendabEdasi))
     if identifikaator == "z":
-        print("\n    Ratva järvele maandus {0} hane ja {1} hane lendab edasi!".format(mituMaandub,lendabEdasi))
+        print("    Ratva järvele maandus {0} hane ja {1} hane lendab edasi!".format(mituMaandub,lendabEdasi))
+
+        
 def kontroll(arv,alles):
     if  0 > alles:
-        print('''
-        Parves {0} ei ole nii palju hanesid!
-
+        print('''        Parves {0} ei ole nii palju hanesid!
         Parves {0} on {1} hane!
-
         Sisestage väiksem väärtus!'''.format(nimedeList[arv],liikmeteArvuList[arv]))
         kontroll = True
     else: kontroll = False
     return kontroll
+
+
 def jatkamine():
     sisend = input('Kas te soovite jätkata(jah/ei)?')
     if sisend == 'jah':
@@ -90,7 +86,7 @@ def jatkamine():
         
 a = True
 while a == True:
-    mituParve = int(input("\nMitu haneparve on nähtud? "))
+    mituParve = int(input("Mitu haneparve on nähtud?    "))
     listid=haneparvenimed()
     nimedeList=listid['nimedeList']
     liikmeteArvuList=listid['liikmeteArvuList']
