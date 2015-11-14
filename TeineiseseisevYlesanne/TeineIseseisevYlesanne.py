920aa2e5ff911dd01be409a4671f182a084b5052
unknown <rene1047@gmail.com>
2015-11-14 20:54:48 +0200

    uus proov

--- TeineiseseisevYlesanne/TeineIseseisevYlesanne.py
+++ TeineiseseisevYlesanne/TeineIseseisevYlesanne.py
@@ -1,113 +1,102 @@
-import string
-import sys
-#---------------------------------------------------------------------------------------------
-def kirjavahemargid(x):
-    a=bool()
-    for char in x:
-        if char in string.punctuation: a=True
-    return a
-def haneparvedjaliikmed(x1,x2,x3,x4,x5,x6):
-    while x1 != x2-1:
-        x6=input('''    Sisestage {0}. haneparve...
-        ...nimi: '''.format(x2))
-        while kirjavahemargid(x6)==True or x6.isdigit() == True or x6 ==''or' 'in x6:
-            x6=input('''    Sisestage {0}. haneparve...
-        ...nimi: '''.format(x2))
-        while x4.isdigit()==False or x4=='0':
-            x4=input('        ...liikmete arv: ')
-        x5.append(x6)
-        x3.append(x4)
-        x2+=1
-        x4=''
-def parveLend(y1,y2,y3,y4,y5):
-    while y1.isdigit() is False or int(y1)>int(y2):
-        y1=input("Mitu hane maandub '{0}' parvest {1}? ".format(y3[indeks],y5))
-        if y1.isdigit() is True:
-    
-            if int(y1)> int(y2):
-                print('''    Parves '{0}' ei ole nii palju hanesid!
-    Parves '{0}' on {1} hane!
-    Sisestage väiksem väärtus!'''.format(y3[indeks],y2))
-    return y1
-def kokkuvote(x,y,z,t):
-    t=map(int,t)
-    eiPeatunud=sum(t)-(x+y+z)
-    c=print('''\nEndla järvele on maandunud kokku {0} hane!
-Saadjärvele on maandunud kokku {1} hane!
-Ratva järvele on maandunud kokku {2} hane!
-Eesti järvedel ei peatunud {3} hane!\n'''.format(x,y,z,eiPeatunud))
-    return c
-#----------------------------------------------------------------------------------------------    
-sisend = 'jah'
+mituParve = int(input("\nMitu haneparve on nähtud? "))
 
-while sisend == 'jah':
-    mituParve=''
-    while not mituParve.isdigit():
-        mituParve = input('Mitu haneparve on nähtud? ')
-    mituParve=int(mituParve)
+def haneparvenimed():
     arv=1
-    nimed=list()
-    liikmeteArv=list()
-    nimedeList='1'
-    LiikmeteArvuList=''
-    
-    haneparvedjaliikmed(mituParve,arv,liikmeteArv,LiikmeteArvuList,nimed,nimedeList)
-    
-    arv=len(nimed)
-    indeks=0
-    print('\nLõunasse lähevad järgmised haneparved: ')
-    while arv != indeks:
-        print("    '{0}' ({1})".format(nimed[indeks],liikmeteArv[indeks]))
-        indeks+=1	
-    indeks=0
-    kokkuRatva=0
-    kokkuEndla=0
-    kokkuSaad=0
-    while len(nimed) != indeks:
-        endlaJarv=''
-        saadJarv=''
-        ratvaJarv=''
-        lendabSaad=0
-        lendabRatva=0
-        lendabEdasi=0
+    nimedeList=list()
+    liikmeteArvuList=list()
+    while arv-1 != mituParve:
+        print("\n    Sisestage {0}. haneparve ...".format(arv))
+        nimi=input("\n        ...nimi: ")
+        liikmeteArv=input("\n        ...liikmete arv: ")
+        nimedeList.append(nimi)
+        liikmeteArvuList.append(liikmeteArv)
+        arv+=1
+    return {'nimedeList':nimedeList, 'liikmeteArvuList':liikmeteArvuList}
 
-        endlaJarv=parveLend(endlaJarv,liikmeteArv[indeks],nimed,lendabSaad,'Endla järvele')
-        lendabSaad=int(liikmeteArv[indeks])-int(endlaJarv)
-        print('    Endla järvele maandus {0} hane ja {1} lendab edasi!'.format(endlaJarv,lendabSaad))
+def lounasseLahevad():
+    print("Lõunasse lähevad järgmised haneparved:\n")
+    arv=0
+    while arv != int(mituParve):
+        print("    ","'{0}'({1}) hane".format(nimedeList[arv],liikmeteArvuList[arv]))
+        arv+=1
+
+def maandumine():
+    arv=0
+    kokkuMaandubEndla = 0
+    kokkuMaandubSaad = 0
+    kokkuMaandubRatva = 0
+    kokkuLendabEdasi = 0
+    print("")
+    while arv != len(nimedeList):
+        mituMaandubEndla = input("Mitu hane maandub '{0}' parvest Endla järvele? ".format(nimedeList[arv]))
+        lendabSaad = int(liikmeteArvuList[arv]) - int(mituMaandubEndla)
+        kontroll(arv,lendabSaad)
+        vastus("x",mituMaandubEndla,lendabSaad)
+        mituMaandubSaad = input("Mitu hane maandub '{0}' parvest Saadjärvele? ".format(nimedeList[arv]))
+        lendabRatva = lendabSaad - int(mituMaandubSaad)
+        kontroll(arv,lendabRatva)
+        vastus("y",mituMaandubSaad,lendabRatva)
+        mituMaandubRatva =input("Mitu hane maandub '{0}' parvest Ratva järvele? ".format(nimedeList[arv]))
+        lendabEedasi = lendabRatva - int(mituMaandubRatva)
+        kontroll(arv,lendabEedasi)
+        vastus("z",mituMaandubRatva,lendabEedasi)
+                                
+        kokkuMaandubEndla = kokkuMaandubEndla + int(mituMaandubEndla)
+        kokkuMaandubSaad = kokkuMaandubSaad + int(mituMaandubSaad)                          
+        kokkuMaandubRatva = kokkuMaandubRatva + int(mituMaandubSaad)
+        kokkuLendabEdasi +=lendabEedasi
+        arv+=1
         
-        saadJarv=parveLend(saadJarv,lendabSaad,nimed,lendabRatva,'Saadjärvele')
-        lendabRatva=int(lendabSaad)-int(saadJarv)
-        print('    Saadjärvele maandus {0} hane ja {1} lendab edasi!'.format(saadJarv,lendabRatva))
-        
-        ratvaJarv=parveLend(ratvaJarv,lendabRatva,nimed,lendabEdasi,'Ratva järvele')
-        lendabEdasi=int(lendabRatva)-int(ratvaJarv)
-        print('    Ratva järvele maandus {0} hane ja {1} hane lendab edasi!'.format(ratvaJarv,lendabEdasi))
+    print('''
 
-        indeks+=1
-        kokkuRatva=kokkuRatva+int(ratvaJarv)
-        kokkuSaad=kokkuSaad+int(saadJarv)
-        kokkuEndla=kokkuEndla+int(endlaJarv)
-    kokkuvote(kokkuEndla,kokkuSaad,kokkuRatva,liikmeteArv)
+Endla järvele on maandunud kokku {0} hane!
 
-    sisend = input('Kas the soovite jätkata(jah/ei)? ')
+Saadjärvele on maandunud kokku {1} hane!
+
+Ratva järvele on maandunud kokku {2} hane!
+
+Eesti järvedel ei peatunud {3} hane!
+'''.format(kokkuMaandubEndla,kokkuMaandubSaad,kokkuMaandubRatva,kokkuLendabEdasi))
+                                
+    return 
+
+def vastus(identifikaator,mituMaandub,lendabEdasi):
+    if identifikaator == "x":
+        print("\n    Endla järvele maandus {0} hane ja {1} hane lendab edasi!".format(mituMaandub,lendabEdasi))                                            
+    if identifikaator == "y":
+        print("\n    Saadjärvele maandus {0} hane ja {1} hane lendab edasi!".format(mituMaandub,lendabEdasi))
+    if identifikaator == "z":
+        print("\n    Ratva järvele maandus {0} hane ja {1} hane lendab edasi!".format(mituMaandub,lendabEdasi))
+def kontroll(arv,alles):
+    if  0 > alles:
+        print('''
+        Parves {0} ei ole nii palju hanesid!
+
+        Parves {0} on {1} hane!
+
+        Sisestage väiksem väärtus!'''.format(nimedeList[arv],liikmeteArvuList[arv]))
+        kontroll = True
+    else: kontroll = False
+    return kontroll
+def jatkamine():
+    sisend = input('Kas te soovite jätkata(jah/ei)? ')
     if sisend == 'jah':
-        print('')
-print('Programm läks kinni!')
-sys.exit
+        print("")
+        edasi = True
+    else:
+        print("Programm läks kinni!")
+a = True
+while a == True:
+    listid=haneparvenimed()
+    nimedeList=listid['nimedeList']
+    liikmeteArvuList=listid['liikmeteArvuList']
+    lounasseLahevad()
+    maandumine()
+    a=jatkamine()
 
     
-    
-    
-
- 
-           
-        
-
-
-        
-
-
-
-    
-
-
+      
+#Haneparve nimi ei tohi sisaldada tühikuid, ega muid kirjavahemärke (nt !, ?, . jne).
+#Parandada tagasisides saadud vead testide lugemise juhendi järgi (vt siia - lisatud Error_OutputIsTooShort ja Error_EOF_InputError veateadete põhjused).
+#Error_EOF_InputError -  Punktide arv ei ole kunagi suurem nullist.
+#Programmi töö võib lõpetada küsimus "Kas soovite jätkata (jah/ei)? ", mille vastuseks on "ei" ja kõik muud vastused peale "jah".
