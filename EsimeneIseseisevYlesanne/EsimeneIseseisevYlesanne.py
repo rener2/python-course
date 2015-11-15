f4cba1f067463622cc6d08faba8518a62fe32ab3
unknown <rene1047@gmail.com>
2015-11-15 22:13:25 +0200

    uus proov

--- EsimeneIseseisevYlesanne/EsimeneIseseisevYlesanne.py
+++ EsimeneIseseisevYlesanne/EsimeneIseseisevYlesanne.py
@@ -2,7 +2,7 @@ rohk =1.65
 import random
 arv=2
 kasKaivitada = input("Süsteem on ülesse seatud - kas käivitada süsteem (jah/ei)? ")
-if kasKaivitada != "jah":
+if kasKaivitada not in "jah":
     print("Süsteemi ei võta tööle!")
 else:
     print("\n\tSüvaveepump käivitus!")
