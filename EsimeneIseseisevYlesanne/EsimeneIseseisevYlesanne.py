40566e2f0b661b54007334ea2ea9b5b33005188f
unknown <rene1047@gmail.com>
2015-11-15 17:17:05 +0200

    uus proov

--- EsimeneIseseisevYlesanne/EsimeneIseseisevYlesanne.py
+++ EsimeneIseseisevYlesanne/EsimeneIseseisevYlesanne.py
@@ -31,6 +31,12 @@ if kasKaivitada == "jah":
                 if veeTarbimine== '[Ei tarbita]':rohuSuund = '[Seisab]'
                 print('\t',pumbaOlek, veeTarbimine, rohuSuund,':','%.2f' %rohk,bar,)                                             
                
-
+            while pumbaOlek == '[Töötab]' and rohk<= 2.75:              
+                veeTarbimine = random.choice(('[Tarbitakse]', '[Ei tarbita]'))
+                rohuSuund = '[Tõuseb]'                                                          
+                if veeTarbimine== '[Ei tarbita]' and rohk <= 2.70 : rohk += 0.15                                                         
+                if veeTarbimine== '[Tarbitakse]'and rohk <= 2.75: rohk += 0.10    
+                rohk = round(rohk,2)
+                print('\t',pumbaOlek, veeTarbimine, rohuSuund,':','%.2f' %rohk,bar)
         
             kasKraanid = input("\nKas te soovite jätkata (jah/ei)? ")           
