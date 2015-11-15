7873d521fac59db8510effd0df4c928d4f50bb67
unknown <rene1047@gmail.com>
2015-11-15 17:19:53 +0200

    uus proov

--- EsimeneIseseisevYlesanne/EsimeneIseseisevYlesanne.py
+++ EsimeneIseseisevYlesanne/EsimeneIseseisevYlesanne.py
@@ -19,24 +19,9 @@ if kasKaivitada == "jah":
         while kasKraanid == 'jah':                                                                                                                                                                 
             
             if 2.75 <= rohk <= 2.85:pumbaOlek= '[Seisab]'                                   
-            elif 1.75 <= rohk <= 1.85:pumbaOlek= '[Töötab]'                                               
+            if 1.75 <= rohk <= 1.85:pumbaOlek= '[Töötab]'                                               
             bar = str('bar')                                                                            
             
-          
-            while pumbaOlek == '[Seisab]' and 1.80 <= rohk:                                                            
-                veeTarbimine= random.choice(('[Tarbitakse]', '[Ei tarbita]')) 
-                if veeTarbimine=='[Tarbitakse]':rohuSuund = '[Alaneb]'
-                if veeTarbimine== '[Tarbitakse]'and 1.80<=rohk: rohk -= 0.05
-                rohk = round(rohk,2)
-                if veeTarbimine== '[Ei tarbita]':rohuSuund = '[Seisab]'
-                print('\t',pumbaOlek, veeTarbimine, rohuSuund,':','%.2f' %rohk,bar,)                                             
-               
-            while pumbaOlek == '[Töötab]' and rohk<= 2.75:              
-                veeTarbimine = random.choice(('[Tarbitakse]', '[Ei tarbita]'))
-                rohuSuund = '[Tõuseb]'                                                          
-                if veeTarbimine== '[Ei tarbita]' and rohk <= 2.70 : rohk += 0.15                                                         
-                if veeTarbimine== '[Tarbitakse]'and rohk <= 2.75: rohk += 0.10    
-                rohk = round(rohk,2)
-                print('\t',pumbaOlek, veeTarbimine, rohuSuund,':','%.2f' %rohk,bar)
+    
         
             kasKraanid = input("\nKas te soovite jätkata (jah/ei)? ")           
