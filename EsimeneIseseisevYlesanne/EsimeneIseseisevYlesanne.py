f4687627bd1a60b9de7d583afa4b8d5145ad3d86
unknown <rene1047@gmail.com>
2015-11-15 20:45:13 +0200

    uus proov

--- EsimeneIseseisevYlesanne/EsimeneIseseisevYlesanne.py
+++ EsimeneIseseisevYlesanne/EsimeneIseseisevYlesanne.py
@@ -1,7 +1,7 @@
 rohk =1.65
 import random
 arv=2
-kasKaivitada = input("Süsteem on ülesse seatud - kas käivitada süsteem (jah/ei)?")
+kasKaivitada = input("Süsteem on ülesse seatud - kas käivitada süsteem (jah/ei)? ")
 if kasKaivitada != "jah":
     print("Süsteemi ei võta tööle!")
 if kasKaivitada == "jah":
@@ -16,22 +16,22 @@ if kasKaivitada == "jah":
     kasKraanid = input("\nKas keerata kraanid lahti (jah/ei)? ")
     if kasKraanid != "jah":
         print("Kraane lahti ei keeratud!")
-    while kasKraanid == 'jah':
+    while kasKraanid == "jah":
         if arv % 2 == 0:
             while 1.85<=rohk:
-                veeTarbimine= random.choice(('[Tarbitakse]', '[Ei tarbita]'))
-                if veeTarbimine=='[Tarbitakse]':rohuSuund = '[Alaneb]'
-                if veeTarbimine== '[Tarbitakse]'and 1.80<=rohk: rohk -= 0.05
+                veeTarbimine= random.choice(("[Tarbitakse]", "[Ei tarbita]"))
+                if veeTarbimine=="[Tarbitakse]":rohuSuund = "[Alaneb]"
+                if veeTarbimine== "[Tarbitakse]"and 1.80<=rohk: rohk -= 0.05
                 rohk = round(rohk,2)
-                if veeTarbimine== '[Ei tarbita]':rohuSuund = '[Seisab]'
-                print("\t[Seisab]", veeTarbimine, rohuSuund,':','%.2f' %rohk,"bar")
+                if veeTarbimine== "[Ei tarbita]":rohuSuund = "[Seisab]"
+                print("\t[Seisab]", veeTarbimine, rohuSuund,":","%.2f" %rohk,"bar")
         if arv % 2 != 0:
             while rohk <= 2.75:
-                veeTarbimine = random.choice(('[Tarbitakse]', '[Ei tarbita]'))                                                        
+                veeTarbimine = random.choice(("[Tarbitakse]", "[Ei tarbita]"))                                                        
                 if veeTarbimine== '[Ei tarbita]' and rohk <= 2.70 : rohk += 0.15                                                         
                 if veeTarbimine== '[Tarbitakse]'and rohk <= 2.75: rohk += 0.10    
                 rohk = round(rohk,2)
-                print("\t[Töötab]", veeTarbimine,'[Tõuseb]',':','%.2f' %rohk,"bar")
+                print("\t[Töötab]", veeTarbimine,"[Tõuseb] : %.2f" %rohk,"bar")
         arv +=1
         kasKraanid = input("\nKas te soovite jätkata (jah/ei)? ")           
         if kasKraanid != "jah":print("Süsteem suleti!")
