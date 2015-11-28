efa1e8dcb9fe230353cd65ae6b7118e69efbe2a7
unknown <rene1047@gmail.com>
2015-11-28 17:49:24 +0200

    parandused

--- EsimeneIseseisevYlesanne/EsimeneIseseisevYlesanne.py
+++ EsimeneIseseisevYlesanne/EsimeneIseseisevYlesanne.py
@@ -6,8 +6,7 @@ kasKaivitada=input("Süsteem on ülesse seatud - kas käivitada süsteem (jah/ei
 if kasKaivitada != "jah":
     print("Süsteemi ei võta tööle!")
 elif kasKaivitada == "jah":
-    print("\n\tSüvaveepump käivitus! ")
-    print("\t\tHüdrofoori rõhk: ")
+    print("\n\tSüvaveepump käivitus!\n\t\tHüdrofoori rõhk:")
     
     while rohk <= 2.80:
         rohk += 0.15
