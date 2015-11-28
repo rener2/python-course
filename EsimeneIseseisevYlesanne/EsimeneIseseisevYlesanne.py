47eca33ef91bede532f99c00100aaa28109083bf
unknown <rene1047@gmail.com>
2015-11-28 17:09:10 +0200

    parandused

--- EsimeneIseseisevYlesanne/EsimeneIseseisevYlesanne.py
+++ EsimeneIseseisevYlesanne/EsimeneIseseisevYlesanne.py
@@ -2,7 +2,7 @@ rohk =1.65
 import random
 arv=2
 
-kasKaivitada = str(input("Süsteem on ülesse seatud - kas käivitada süsteem (jah/ei)? "))
+kasKaivitada=input('Süsteem on ülesse seatud - kas käivitada süsteem (jah/ei)?')
 
 if kasKaivitada != "jah":
     print("Süsteemi ei võta tööle!")
