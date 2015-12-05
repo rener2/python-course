a4561add3804830cb15b34e0d3dc5927a46e194b
unknown <rene1047@gmail.com>
2015-12-05 19:56:45 +0200

    tester

--- EsimeneIseseisevYlesanne/EsimeneIseseisevYlesanne.py
+++ EsimeneIseseisevYlesanne/EsimeneIseseisevYlesanne.py
@@ -2,16 +2,15 @@ rohk =1.65
 import random
 arv=2
 kasKaivitada = input("Süsteem on ülesse seatud - kas käivitada süsteem (jah/ei)?")
-if kasKaivitada.lower() != "jah":
+if kasKaivitada != "jah":
     print("Süsteemi ei võta tööle!")
     
-elif kasKaivitada.lower() == "jah":
+elif kasKaivitada == "jah":
     print("\n\tSüvaveepump käivitus!\n\t\tHüdrofoori rõhk:")
-    
     while rohk <= 2.80:
         rohk += 0.15
         rohk = "%.2f"%rohk
-        print("\t\t\t{0} bar".format(rohk))
+        print("\t\t\t{0}bar".format(rohk))
         rohk = float(rohk)
     print("\tSüvaveepump seiskus!")
     kasKraanid = input("\nKas keerata kraanid lahti (jah/ei)? ")
