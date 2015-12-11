3919df7e6701cecacf88339f738bc63192856b9f
unknown <rene1047@gmail.com>
2015-12-11 17:07:34 +0200

    testin testerit

--- KolmasIseseisevYlesanne/KolmasIseseisevYlesanne.py
+++ KolmasIseseisevYlesanne/KolmasIseseisevYlesanne.py
@@ -23,7 +23,7 @@ def lisa_salvesta():
         return valikvastus()
     if check_brackets(sisend)==False: return lisa_salvesta()
     fail=open("andmebaas.txt","a",encoding="utf-8")
-    fail.write(aeg+" - "+sisend+"\t\n")
+    fail.write(aeg+" - "+sisend+"\n")
     fail.close()
     print("\tSade fikseeritud!\t\n")
     valik=valikvastus()
@@ -35,7 +35,7 @@ def muuda(muudetava_indeks):
     sisend,aeg=input("\tFikseerige sade kujul [Identifikaator] [20] (C): "),strftime("%d.%m.%Y %H:%M:%S", localtime())
     if sisend == "C" or sisend == "c": return sisend
     if check_brackets(sisend) == False: return muuda(muudetava_indeks)
-    sisend=aeg+" - "+sisend+"\t\n"
+    sisend=aeg+" - "+sisend+"\n"
     fail=open("andmebaas.txt","r",encoding="utf-8")
     sisu=fail.readlines()
     sisu.pop(muudetava_indeks)
