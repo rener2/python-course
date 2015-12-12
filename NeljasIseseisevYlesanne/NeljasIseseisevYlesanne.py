4e8fd52a5c293a79a84db6fce7f0993aa167a6f9
unknown <rene1047@gmail.com>
2015-12-12 23:15:43 +0200

    stiiliparandused

--- NeljasIseseisevYlesanne/NeljasIseseisevYlesanne.py
+++ NeljasIseseisevYlesanne/NeljasIseseisevYlesanne.py
@@ -3,7 +3,9 @@ __author__ = 'Rene Räkk'
 
 
 from tkinter import Canvas, PhotoImage, Tk, Label, Button
+"""This module is used for creating images, windows, text and a button"""
 import random
+"""This module is used for a random choice between colors"""
 
 
 def reklaam_start(taust):
